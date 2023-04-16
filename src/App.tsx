82f8b93d3ffa6220d2358ed07b8422ca61ec2af6
Willians Santos <taldowill29@gmail.com>
2023-04-16 04:02:19 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '.././styles/index.sass'
+
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../styles/components/Header.sass'
+
+function Header() {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>Início</li>
+                <li>Sobre</li>
+                <li>Tecnologias</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
