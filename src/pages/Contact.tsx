e78dcc4886d8d9c9a66142aede4ff97eef083565
Willians Santos <taldowill29@gmail.com>
2023-04-20 01:00:31 -0400

    fix contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
   return (
     <main className='contact-page'>
       <h1>Entre em contato diretamente comigo!</h1>
-      <form id='contact-form' action="https://formsubmit.co/e32134f4ee91c22f07d642eb9c82f02e" method="POST">
+      <form id='contact-form' action="https://formsubmit.co/o.santxz@proton.me" method="POST">
         <div className='contact-inputs-container'>
           <input onInvalid={invalidInput} type="text" name='Nome' id='contact-input-firstname' placeholder='Primeiro nome' maxLength={20} minLength={3} required />
           <input onInvalid={invalidInput} type="text" name='Sobrenome' id='contact-input-lastname' placeholder='Segundo nome' maxLength={36} minLength={3} required />
@@ -28,7 +28,7 @@ const Contact = () => {
         <textarea name="Mensagem" id="contact-textarea-message" cols={20} rows={10} placeholder='Digite sua mensagem'/>
         <button type='submit'>ENVIAR <FaPaperPlane/></button>
         <input type="hidden" name="_captcha" value="false"/>
-        <input type="hidden" name="_next" value="http://localhost:5173/sucesso"></input>
+        <input type="hidden" name="_next" value="https://dev-portfolio-will-santosx.vercel.app/sucesso"></input>
       </form>
     </main>
   )
